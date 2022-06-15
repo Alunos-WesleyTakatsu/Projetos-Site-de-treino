5d4e9b13403201576a9702c0b5cdc97d0833927e
Alunos-WesleyTakatsu <107277184+Alunos-WesleyTakatsu@users.noreply.github.com>
2022-06-15 16:01:41 -0300

    Aula 2
    Colocamos mais uma página de filmes, organizamos os arquivos nas pastas, e introduzimos ao JavaScript.

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+var resultado;
+
+function mensagem () {
+    var numero1 = parseInt(prompt("Digite um número","5"));
+    var numero2 = parseInt(prompt("Digite outro número","10"));
+    var numero3 = parseInt(prompt("Digite mais um número","15"));
+    resultado = numero1 + numero2 + numero3;
+    alert("O resultado da conta é " + resultado);
+}
+
+function nomesobrenome() {
+    var nome = prompt("digite seu nome");
+    var sobrenome = prompt("digite seu sobrenome");
+    alert("seu nome completo é: " + nome + " " + sobrenome);
+}
+
+alert("Olá! Seja bem vindo!");
+
